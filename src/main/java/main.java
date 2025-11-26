fe32a44d5fc76aa59c3663d098f425126bb5d2f2
eriinys <eriinys@duck.com>
2025-11-26 09:12:36 -0600

    added sql query with try with resources

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        String user = "root";
+        String password = "yearup";
+
+        String sql = "SELECT ProductName FROM products";
+
+        try(Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                String ProductName = rs.getString("ProductName");
+                System.out.println(ProductName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
