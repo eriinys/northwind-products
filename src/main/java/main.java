eb0a1a9764063584fd8e255c9c18121665cd11f9
eriinys <eriinys@duck.com>
2025-11-27 16:22:39 -0600

    implemented MysqlDataSource instead of using DriveManager

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,15 +1,27 @@
 import java.sql.*;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        if (args.length != 2) {
+            System.out.println("Application needs two arguments to run.");
+            System.exit(1); //exits program when required arguments are not provided with exit code 1
+        }
+
+        MysqlDataSource dataSource = new MysqlDataSource();
 
         String url = "jdbc:mysql://localhost:3306/northwind";
-        String user = "root";
-        String password = "yearup";
+        String user = args[0];
+        String password = args[1];
 
         String sql = "SELECT ProductName FROM products";
 
-        try(Connection conn = DriverManager.getConnection(url, user, password);
+        dataSource.setURL(url);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        try(Connection conn = dataSource.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery()) {
 
@@ -22,3 +34,21 @@ public class main {
         }
     }
 }
+
+ /*
+         try(Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement ps = conn.prepareStatement(sql + " WHERE ProductName LIKE ?")) {
+
+            ps.setString(1, "p%");
+
+            try(ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    String ProductName = rs.getString("ProductName");
+                    System.out.println(ProductName);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+         */
